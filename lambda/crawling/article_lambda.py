b3d31acf998588fba600ffc78a03b03bf054cb9e
Ubuntu <ubuntu@ip-172-31-25-141.eu-west-3.compute.internal>
2020-05-23 09:18:30 +0000

    Add cookies for HLN / De Morgen

--- lambda/crawling/article_lambda.py
+++ lambda/crawling/article_lambda.py
@@ -3,9 +3,25 @@ import boto3
 import os
 from pymongo import MongoClient
 from secrets import get_secret
-from newspaper import Article
+from newspaper import Article, fulltext
 import random
+import requests
 
+def get_cookie(url):
+    if "hln.be" in url or "demorgen.be" in url:
+        return "pwv=1; pws=functional"
+    
+    return None
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive',
+}
 
 secret_client = boto3.client("secretsmanager")
 
@@ -17,20 +33,32 @@ db = MongoClient(secrets["mongo_host"], username=secrets["mongo_user"], password
 def handler(event=None, context=None):
     url = event["url"]
 
-    article = Article(url)
-    article.download()
-    article.parse()
+    cookie = get_cookie(url)
+    if cookie is not None:
+        headers["Cookie"] = cookie
+       
+        html = requests.get(url, headers=headers).text
+      
+        
+        db["articles"].update_one({"url": url}, {"$set": {
+            "text": text
+        }})
+   
+    else:
+        article = Article(url)
+        article.download()
+        article.parse()
 
-    db["articles"].update_one({"url": url}, {"$set":{
-        "text": article.text,
-        "title": article.title,
-        "meta.newspaper3k": {
-            "authors": article.authors,
-            "summary": article.summary,
-            "top_image": article.top_image,
-            "date": article.publish_date
-        }
-    }})
+        db["articles"].update_one({"url": url}, {"$set":{
+            "text": article.text,
+            "title": article.title,
+            "meta.newspaper3k": {
+                "authors": article.authors,
+                "summary": article.summary,
+                "top_image": article.top_image,
+                "date": article.publish_date
+            }
+        }})
 
 def reprocess_empty_articles():
     urls = db["articles"].find({"text":{"$in":["", None]}}).distinct("url")
@@ -49,4 +77,4 @@ def reprocess_empty_articles():
 if __name__ == "__main__":
 
     reprocess_empty_articles()
-
+    
