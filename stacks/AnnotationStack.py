b903e50ad0d7e8db409c8905287a1802e777efdf
Louis de Viron <louis@datatext.eu>
2022-08-18 13:19:03 +0200

    Update to CDK v2

--- stacks/AnnotationStack.py
+++ stacks/AnnotationStack.py
@@ -1,46 +1,58 @@
-from aws_cdk import core, aws_lambda, aws_secretsmanager, aws_apigateway, aws_events, aws_events_targets
-from stacks.stack_utils import *
+from constructs import Construct
+from aws_cdk import Stack, Duration
+from aws_cdk import aws_lambda
+from aws_cdk import aws_secretsmanager
+from aws_cdk import aws_apigateway
+from aws_cdk import aws_events, aws_events_targets
 
 
-class AnnotationStack(core.Stack):
+class AnnotationStack(Stack):
     
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
 
         super().__init__(scope, id, **kwargs)
 
         code_path = "./lambda/annotation"
+    
 
-        """ Define Layers """
-        
-        requests_layer = get_layer(self, "requests")
-        pymongo_layer = get_layer(self, "pymongo")
-
-        secret = get_secret(self)
+        secret = aws_secretsmanager.Secret.from_secret_attributes(
+            scope=self,
+            id="media-analysis-secret",
+            secret_partial_arn=f"arn:aws:secretsmanager:eu-west-3:771823009556:secret:media_analysis_secret"
+        )
         
         """ Define Lambdas """
         
         # TR unique
-        textrazor_lambda = get_lambda(
+        textrazor_lambda = PythonFunction(
             scope=self,
-            name="textrazor_lambda",
-            env_dict={
+            id=f"media-analysis-textrazor-lambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            function_name=f"media-analysis-textrazor-lambda",
+            entry=code_path,
+            index="textrazor_lambda.py",
+            environment={
                 "SECRET_ARN": secret.secret_arn,
             },
-            layers=[pymongo_layer, requests_layer],
-            code_path=code_path,
+            timeout=Duration.seconds(900),
+            memory_size=3008,
             reserved_concurrent_executions=4
         )
 
-        # RT on DB
-        annotation_lambda = get_lambda(
+        # TR on DB
+        annotation_lambda = PythonFunction(
             scope=self,
-            name="annotation_lambda",
-            env_dict={
+            id=f"media-analysis-annotation-lambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            function_name=f"media-analysis-annotation-lambda",
+            entry=code_path,
+            index="annotation_lambda.py",
+            environment={
                 "SECRET_ARN": secret.secret_arn,
-                "TEXTRAZOR_LAMBDA": textrazor_lambda.function_name
             },
-            layers=[pymongo_layer, requests_layer],
-            code_path=code_path,
+            timeout=Duration.seconds(900),
+            memory_size=3008,
+            reserved_concurrent_executions=4
         )
 
         secret.grant_read(textrazor_lambda)
@@ -52,8 +64,8 @@ class AnnotationStack(core.Stack):
         # Cron every 4 hours
         aws_events.Rule(
             scope=self,
-            id='annotation-cron',
-            schedule=aws_events.Schedule.rate(core.Duration.hours(4)),
+            id='media-analysis-annotation-cron',
+            schedule=aws_events.Schedule.rate(Duration.hours(4)),
             targets=[aws_events_targets.LambdaFunction(annotation_lambda)]
         )
         
